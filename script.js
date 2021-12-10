481b1ecfff7fb320e1c19773ad4e979522a60642
Rene-Lee Sylvain <RenSylvain@gmail.com>
2021-12-09 18:01:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let avg;
 let total = 0;
 
 for(let i = 0; i < heroLvls.length; i++) {
-	total = total + heroLvls[i].textContent
-	if(i == (heroLvls.length - 1){
+	total = total + parseInt(heroLvls[i].textContent)
+	if(i == (heroLvls.length - 1)){
 		avg = (total/(i+1)).toFixed(1);
 	}
 }
